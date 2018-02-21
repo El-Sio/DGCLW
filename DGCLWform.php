eb81dfb6a415e2f24d4808daab304caa046e5e2f
Charles LESOIL <elsiokun@gmail.com>
2018-02-21 23:52:30 +0100

    version 2.5
    added duration for audio tracks with formatting in <min>:<sec> with 0s
    take correct duration tag for audio and video

--- DGCLWform.php
+++ DGCLWform.php
@@ -172,6 +172,27 @@ if ($handle = opendir('./xmlin/')) {
         return frag;
     }
 
+function parseduration(str) {
+    var res = str.split("PT");
+    var res2 = res[1].split("M");
+    if(res2[0].length == res[1].length) {
+    var res3 = res[1].split("S");
+    var zero ="";
+    if(res3[0].length == 1) {zero = "0";}
+    var result = "00:"+zero+res3[0];
+    }
+    else {
+    var res3 = res2[1].split("S");
+    var zero="";
+    var zerom = "";
+    if(res3[0].length == 1) {zero = "0";}
+    if(res3[0].length == 0) {zero = "00";}
+    if(res2[0].length == 1) {zerom = "0";}
+    var result = zerom+res2[0]+":"+zero+res3[0];
+    }
+    return result;
+}
+    
     function removetracks() {
         var paras = document.getElementsByClassName('tracks');
 
@@ -218,6 +239,7 @@ if ($handle = opendir('./xmlin/')) {
             var text = tracklist[i].getElementsByTagName("SoundRecordingDetailsByTerritory")[0].getElementsByTagName("TitleText")[index].childNodes[0].nodeValue;
             var trkfilename = tracklist[i].getElementsByTagName("SoundRecordingDetailsByTerritory")[0].getElementsByTagName("FileName")[0].childNodes[0].nodeValue;
             var artist = tracklist[i].getElementsByTagName("FullName")[0].childNodes[0].nodeValue;
+            var duration = parseduration(tracklist[i].getElementsByTagName("Duration")[0].childNodes[0].nodeValue);
         }
         if(video) {
             var titles = tracklist[i].getElementsByTagName("VideoDetailsByTerritory")[0].getElementsByTagName("Title");
@@ -235,24 +257,21 @@ if ($handle = opendir('./xmlin/')) {
             var trkfilename = tracklist[i].getElementsByTagName("VideoDetailsByTerritory")[0].getElementsByTagName("FileName")[index_codec].childNodes[0].nodeValue;
             var bitrate = tracklist[i].getElementsByTagName("VideoDetailsByTerritory")[0].getElementsByTagName("VideoBitRate")[index_codec].childNodes[0].nodeValue;
             var framerate = tracklist[i].getElementsByTagName("VideoDetailsByTerritory")[0].getElementsByTagName("FrameRate")[index_codec].childNodes[0].nodeValue;
-            
-            if(!tracklist[i].getElementsByTagName("VideoDetailsByTerritory")[0].getElementsByTagName("TechnicalVideoDetails")[index_codec].getElementsByTagName("Duration").isEmptyObject) {
-               var duration = tracklist[i].getElementsByTagName("VideoDetailsByTerritory")[0].getElementsByTagName("TechnicalVideoDetails")[index_codec].getElementsByTagName("Duration")[0].childNodes[0].nodeValue;
-        } else {var duration = "N/A";}
+            var duration = parseduration(tracklist[i].getElementsByTagName("Duration")[0].childNodes[0].nodeValue);
         }
         var generateHere = document.getElementById("dataform");
-        var frag = create("<li class='tracks'><label>Track "+(i+1)+": </label><input type='text' id='track"+(i+1)+"' name='track_"+(i+1)+"'/><li class='tracks'><label>Artist : </label><input type='text' id='artist"+(i+1)+"' name='artist_"+(i+1)+"'/></li><li class='tracks'><label>File Name "+(i+1)+": </label><input class='readonly' type='text' id='file"+(i+1)+"' name='file_"+(i+1)+"' readonly='readonly' /></li>");
+        var frag = create("<li class='tracks'><label>Track "+(i+1)+": </label><input type='text' id='track"+(i+1)+"' name='track_"+(i+1)+"'/><li class='tracks'><label>Artist : </label><input type='text' id='artist"+(i+1)+"' name='artist_"+(i+1)+"'/></li><li class='tracks'><label>Duration :</label><input type='text' class='readonly' id='duration"+(i+1)+"' name='duration_"+(i+1)+"'/></li><li class='tracks'><label>File Name "+(i+1)+": </label><input class='readonly' type='text' id='file"+(i+1)+"' name='file_"+(i+1)+"' readonly='readonly' /></li>");
         generateHere.insertBefore(frag, document.getElementById("last-item"));
         if(video) {
-            var frag_tech = create("<li class='tracks'><label>Video Bitrate (kBps) :</label><input class='readonly' type='text' id='bitrate"+(i+1)+"' name='bitrate_"+(i+1)+"'/></li><li class='tracks'><label>Video Framerate (Hz) :</label><input type='text' class='readonly' id='framerate"+(i+1)+"' name='framerate_"+(i+1)+"'/></li><li class='tracks'><label>Duration :</label><input type='text' class='readonly' id='duration"+(i+1)+"' name='duration_"+(i+1)+"'/></li>");
+            var frag_tech = create("<li class='tracks'><label>Video Bitrate (kBps) :</label><input class='readonly' type='text' id='bitrate"+(i+1)+"' name='bitrate_"+(i+1)+"'/></li><li class='tracks'><label>Video Framerate (Hz) :</label><input type='text' class='readonly' id='framerate"+(i+1)+"' name='framerate_"+(i+1)+"'/></li>");
             generateHere.insertBefore(frag_tech, document.getElementById("last-item"));
             $("#bitrate"+(i+1)).val(bitrate);
             $("#framerate"+(i+1)).val(framerate);
-            $("#duration"+(i+1)).val(duration);
         }
         $("#track"+(i+1)).val(text);
         $("#file"+(i+1)).val(trkfilename);
         $("#artist"+(i+1)).val(artist);
+        $("#duration"+(i+1)).val(duration);
       }
 }
     </script>
